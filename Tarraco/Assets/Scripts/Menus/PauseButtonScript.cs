34264cfe1fa319b3b6cdb6e93a3a04a3b5cfbe44
Fenomanu <61010890+Fenomanu@users.noreply.github.com>
2021-11-28 18:56:21 +0100

    a

--- Tarraco/Assets/Scripts/Menus/PauseButtonScript.cs
+++ Tarraco/Assets/Scripts/Menus/PauseButtonScript.cs
@@ -21,11 +21,7 @@ public class PauseButtonScript : MonoBehaviour
 
     public void Exit()
     {
-<<<<<<< Updated upstream
         Time.timeScale = 1f;
-=======
-        Time.timeScale = 1;
->>>>>>> Stashed changes
         SceneManager.LoadScene(0);
     }
 }
